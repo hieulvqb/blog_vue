88276e33b7564a165553d9a507adf95278511d14
hieuulv <hieuu.dev99@gmail.com>
2020-08-07 01:26:29 +0700

    init project vuejs

---
+++ src/components/Contact.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Contact page</h2>
+    <router-link to="/contact">Contact</router-link>
+    <router-link to="/news">News</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome contact'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      With Bootstrap!
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <router-link to="/contact">Contact</router-link>
+    <router-link to="/news">News</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App b'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+</style>
